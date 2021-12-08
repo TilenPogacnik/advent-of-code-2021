5248520238bca83c159c4fc19a1ddcc598e52cfd
Tilen Pogacnik <tilenpogacnik94@gmail.com>
2021-12-08 14:27:50 +0100

    Day 8 part 2

---
+++ Day8/part2.js
@@ -0,0 +1,40 @@
+var fs = require("fs");
+
+//Explanation for digit groups: https://docs.google.com/spreadsheets/d/1U_tBsShi6tGHjMuMFc9h0KeTiqPcDLVV3C6U3p_RpSE/
+const DIGIT_GROUPS = [ "2112", "2000", "1112", "2111", "2020", "1121", "1122", "2100", "2122", "2121"];
+
+const input = fs.readFileSync("./input.txt").toString('utf-8').split("\n")
+
+const result = input.reduce((prev, curr) => prev += solveEntry(curr), 0);
+console.log("Part 2 solution: " + result);
+
+function solveEntry(entry){
+    var line = entry.split("|").map(_ => _.trim());
+    var groups = generateGroups(line[0].split(" "));
+    return decodeOutput(line[1], groups);
+}
+
+function generateGroups(patterns){
+    var groups = [];
+    groups.push(patterns.find(p => p.length == 2));
+    groups.push(patterns.find(p => p.length == 3).split('').filter(c => !groups[0].includes(c)).join(''));
+    groups.push(patterns.find(p => p.length == 4).split('').filter(c => !groups[0].includes(c)).join(''));
+    groups.push("abcdefg".split('').filter(c => !groups.join('').includes(c)).join(''));
+    return groups;
+}
+
+function decodeOutput(output, groups){
+    const decodeDigit = (encodedDigit) => {
+        var groupCode = new Array(4).fill(0);
+        [...encodedDigit].forEach(char => {
+            groupCode[getCharGroup(char, groups)]++;
+        });
+        return DIGIT_GROUPS.findIndex(_ => _ === groupCode.join(""));       
+    }
+
+    return parseInt(output.split(" ").map(encodedDigit => decodeDigit(encodedDigit)).join(''));
+}
+
+function getCharGroup(char, groups){
+   return groups.findIndex(group => group.includes(char));
+}

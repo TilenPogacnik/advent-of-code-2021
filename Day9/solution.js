7a27c7e5ae514e985b7c40b93d56964d59287752
Tilen Pogacnik <tilenpogacnik94@gmail.com>
2021-12-09 23:21:54 +0100

    Day 9 part 2

--- Day9/solution.js
+++ Day9/solution.js
@@ -1,11 +1,16 @@
 var fs = require("fs");
+const MAX_HEIGHT = 9;
 const input = fs.readFileSync("./input.txt").toString('utf-8');
-
 const heightmap = input.split("\n").map(_ => _.split("").map(_ => parseInt(_)));
 const lowPoints = findLowPoints(heightmap);
 
 var riskLevelSum = lowPoints.reduce((prev, curr) => {return prev + 1 + heightmap[curr[0]][curr[1]]}, 0);
 console.log("Part 1 solution: " + riskLevelSum);
+
+var basinMap = heightmap.slice();
+const basinSizes = lowPoints.map(lp => getBasinSize(lp));
+var largestBasinsMultiplied = basinSizes.sort((a,b) => {return b - a}).slice(0, 3).reduce((prev, curr) => prev * curr, 1);
+console.log("Part 2 solution: " + largestBasinsMultiplied);
 
 function findLowPoints(heightmap){
     var lowPoints = [];
@@ -26,4 +31,27 @@ function isLowPoint(x, y){
     && (y == heightmap.length - 1 || heightmap[y + 1][x] > height)
     && (x == 0 || heightmap[y][x - 1] > height)
     && (x == heightmap[y].length - 1 || heightmap[y][x + 1] > height)
+}
+
+function getBasinSize(point){
+    const isInsideBasin = point => {
+        return point[0] >= 0 
+            && point[0] < basinMap.length 
+            && point[1] >= 0 
+            && point[1] < basinMap[point[0]].length
+            && basinMap[point[0]][point[1]] < MAX_HEIGHT 
+            && basinMap[point[0]][point[1]] >= 0;
+    };
+
+    if (!isInsideBasin(point)){
+        return 0;
+    }
+
+    basinMap[point[0]][point[1]] = -1;
+
+    return 1 
+        + getBasinSize([point[0] + 1, point[1]])
+        + getBasinSize([point[0] - 1, point[1]])
+        + getBasinSize([point[0], point[1] + 1])
+        + getBasinSize([point[0], point[1] - 1]);
 }
